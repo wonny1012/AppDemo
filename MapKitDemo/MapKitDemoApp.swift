51a6c5e992f4ae9c30ef6eb1aa09841d7a8753d3
wonny1012 <rnjswldnjs1012@gmail.com>
2024-02-02 10:51:11 +0900

    Add: 파일연결

---
+++ MapKitDemo/MapKitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKitDemoApp.swift
+//  MapKitDemo
+//
+//  Created by kwon ji won on 2/2/24.
+//
+
+import SwiftUI
+
+@main
+struct MapKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
